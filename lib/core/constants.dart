76155bc9a5ae5ad2edb13866844d945b945733fb
Ahmed Zakarya <ahmedzrks123@gmail.com>
2023-12-13 00:28:25 +0200

    update Constant File

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 
 GlobalKey<FormState> formkey = GlobalKey();
+const Color outlineBorderColor = Color.fromARGB(255, 223, 222, 222);
+    const buttonColor = Color(0xFF34D49E);
+const Color backgroundColor = Colors.white;
 const Color fontprimaryColor = Color.fromRGBO(23, 43, 77, 1);
 const Color fontSecondaryColor = Color.fromARGB(255, 122, 134, 154);
 const Color secondaryColor = Color.fromARGB(255, 239, 240, 241);
 const String fontFamilyString = 'DINNextLTW23';
 const Color linksColor = Color.fromARGB(255, 250, 132, 70);
-  Color errorColor = const Color.fromARGB(255, 202, 31, 18);
+Color errorColor = const Color.fromARGB(255, 202, 31, 18);
 const requiredSign = Text(
   " *",
   style: TextStyle(
@@ -15,12 +18,13 @@ const requiredSign = Text(
     fontFamily: fontFamilyString,
   ),
 );
-  Text label(String text) {
-    return Text(
-                  text,
-                  style:const  TextStyle(
-                      color: fontSecondaryColor,
-                      fontWeight: FontWeight.bold,
-                      fontFamily:fontFamilyString,),
-                );
-  }
+Text label(String text) {
+  return Text(
+    text,
+    style: const TextStyle(
+      color: fontSecondaryColor,
+      fontWeight: FontWeight.bold,
+      fontFamily: fontFamilyString,
+    ),
+  );
+}
